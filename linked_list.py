7104233b0d81415cda71c518bd7c2739d1f6141e
samea <sameanoniM@gmail.com>
2023-03-17 12:34:19 +0300

    modified:   .gitignore new file:   linked_list.py modified:   tests/tests_third.py

---
+++ linked_list.py
@@ -0,0 +1,45 @@
+# Реализация класса Node
+class Node:
+    """ Это узел """
+    def __init__(self, data, next_node=None):
+        self.data = data  # тут данные
+        self.next_node = next_node # тут ссылка на следующий
+
+# Реализация класса LinkedList
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # Метод для добавления узла в начало связанного списка
+    def insert_beginning(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+
+    # Метод для добавления узла в конец связанного списка
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+    # Метод для вывода данных из связанного списка
+    def print_ll(self):
+        ll_string = ''
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f' {str(node.data)} ->'
+            node = node.next_node
+
+        ll_string += ' None'
+        print(ll_string)
--- tests/tests_third.py
+++ tests/tests_third.py
@@ -1,6 +1,7 @@
 import unittest
 from third import Node, Stack
 from custom_queue import Queue
+from linked_list import LinkedList
 
 class TestStack(unittest.TestCase):
     def setUp(self):
@@ -67,6 +68,41 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(self.queue.head.data, 3)
         self.assertEqual(self.queue.tail.data, 3)
         self.assertEqual(self.queue.size, 1)
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.ll = LinkedList()
+
+    def test_insert_beginning(self):
+        self.ll.insert_beginning(1)
+        self.assertEqual(self.ll.head.data, 1)
+        self.assertEqual(self.ll.tail.data, 1)
+
+        self.ll.insert_beginning(2)
+        self.assertEqual(self.ll.head.data, 2)
+        self.assertEqual(self.ll.tail.data, 1)
+
+        self.ll.insert_beginning(3)
+        self.assertEqual(self.ll.head.data, 3)
+        self.assertEqual(self.ll.tail.data, 1)
+
+    def test_insert_at_end(self):
+        self.ll.insert_at_end(1)
+        self.assertEqual(self.ll.head.data, 1)
+        self.assertEqual(self.ll.tail.data, 1)
+
+        self.ll.insert_at_end(2)
+        self.assertEqual(self.ll.head.data, 1)
+        self.assertEqual(self.ll.tail.data, 2)
+
+        self.ll.insert_at_end(3)
+        self.assertEqual(self.ll.head.data, 1)
+        self.assertEqual(self.ll.tail.data, 3)
+
+    def test_print_ll(self):
+        self.ll.insert_beginning(1)
+        self.ll.insert_beginning(2)
+        self.ll.insert_beginning(3)
 
 if __name__ == "__main__":
     unittest.main()
